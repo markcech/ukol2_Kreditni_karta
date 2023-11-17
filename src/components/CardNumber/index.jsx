2d5dce0fcf8f53e29491160b4035063ae56bf105
Marketa Cechova <marketa.sebestova@gmail.com>
2023-11-18 00:26:44 +0100

    first commit

---
+++ src/components/CardNumber/index.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import "./style.css";
+
+const CardNumber = () => {
+  const cardInputRefs = [useRef(), useRef(), useRef(), useRef()];
+
+  const handleInputChange = (e, index) => {
+    const value = e.target.value;
+
+    if (value.length === 4 && index < 3) {
+      cardInputRefs[index + 1].current.focus();
+    }
+  };
+
+  return (
+    <div className="container">
+      <p>Vložte číslo vaší kreditní karty:</p>
+      <div className="credit-card-input">
+        {cardInputRefs.map((cardInputRef, index) => (
+          <input
+            className={`cardInputRef_${index}`}
+            ref={cardInputRef}
+            type="text"
+            maxLength="4"
+            onChange={(e) => handleInputChange(e, index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardNumber;
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,10 @@
+import CardNumber from '../../components/CardNumber';
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <CardNumber/>
+    </div>
+  );
+};
